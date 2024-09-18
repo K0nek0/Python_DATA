bf2fa3b70730704a13134eb81b1141e0433004c0
Timoshchenko Nikolay <silent_master@mail.ru>
2024-09-18 21:15:25 +0200

    task_5

---
+++ lab_2/task_5/main.py
@@ -0,0 +1,22 @@
+import os
+from PIL import Image
+from sys import argv
+
+
+def create_thumbnails(directory, extension):
+    for filename in os.listdir(directory):
+        if filename.lower().endswith(extension.lower()):
+            file_path = os.path.join(directory, filename)
+
+            with Image.open(file_path) as img:
+                img = img.resize((50, 50))
+                img.show()
+                img.save(f'mini_{extension}')
+
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print('Usage: python main.py <file_extension>')
+    else:
+        file_extension = argv[1]
+        create_thumbnails('.', file_extension)
