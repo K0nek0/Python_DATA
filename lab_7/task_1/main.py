3ae0e1ac189749a776644bec25a1dec9f2ba28df
Timoshchenko Nikolay <silent_master@mail.ru>
2024-11-03 20:50:43 +0200

    oops

--- lab_7/task_1/main.py
+++ lab_7/task_1/main.py
@@ -2,7 +2,7 @@ from jsonschema import validate
 import json
 
 
-def validate_xml(invalid_json_file, schema_file):
+def validate_json(invalid_json_file, schema_file):
     with open(invalid_json_file) as json_file, open(schema_file) as schema:
         try:
             validate(json.load(json_file), json.load(schema))
@@ -14,4 +14,4 @@ def validate_xml(invalid_json_file, schema_file):
 if __name__ == '__main__':
     schema_file = "ex_1_shema.json"
     invalid_json_file = "ex_1_invalid.json"
-    validate_xml(invalid_json_file, schema_file)
+    validate_json(invalid_json_file, schema_file)
